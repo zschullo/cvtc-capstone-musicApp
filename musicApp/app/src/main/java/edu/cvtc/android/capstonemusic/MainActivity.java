fac1435bf2c26a6bb8328192919161c07d60de3a
Corey Leavitt <silverwolf5982@gmail.com>
2017-11-05 19:21:11 -0600

    Started to link buttons to code.

--- musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MainActivity.java
+++ musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MainActivity.java
@@ -2,8 +2,16 @@ package edu.cvtc.android.capstonemusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton playButton = (ImageButton) findViewById(R.id.playButton);
+    private ImageButton stopButton = (ImageButton) findViewById(R.id.stopButton);
+    private ImageButton fastForwardButton = (ImageButton) findViewById(R.id.fastForwardButton);
+    private ImageButton reverseButton = (ImageButton) findViewById(R.id.reverseButton);
+    private ImageButton muteButton = (ImageButton) findViewById(R.id.muteButton);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
