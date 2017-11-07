7336f9ce48b69820db0fa12e6ad78d9d03d54044
Corey Leavitt <silverwolf5982@gmail.com>
2017-11-06 22:37:19 -0600

    Added a toast.

--- musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MainActivity.java
+++ musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MainActivity.java
@@ -1,21 +1,60 @@
 package edu.cvtc.android.capstonemusic;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageButton playButton = (ImageButton) findViewById(R.id.playButton);
-    private ImageButton stopButton = (ImageButton) findViewById(R.id.stopButton);
-    private ImageButton fastForwardButton = (ImageButton) findViewById(R.id.fastForwardButton);
-    private ImageButton reverseButton = (ImageButton) findViewById(R.id.reverseButton);
-    private ImageButton muteButton = (ImageButton) findViewById(R.id.muteButton);
+    // TODO: Remove unused buttons after Drew removes them.
+    private ImageButton playButton;
+    //private ImageButton stopButton;
+    private ImageButton fastForwardButton;
+    private ImageButton reverseButton;
+    //private ImageButton muteButton;
+    private Toast toast = null;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Gets the id after the main activity is created.
+        playButton = (ImageButton) findViewById(R.id.playButton);
+        //stopButton = (ImageButton) findViewById(R.id.stopButton);
+        fastForwardButton = (ImageButton) findViewById(R.id.fastForwardButton);
+        reverseButton = (ImageButton) findViewById(R.id.reverseButton);
+        //muteButton = (ImageButton) findViewById(R.id.muteButton);
+
+        playButton.setOnClickListener(this);
+        fastForwardButton.setOnClickListener(this);
+
+    }
+
+    // This will get fired off when you click play.
+    @Override
+    public void onClick(View view) {
+
+        if (view == fastForwardButton) {
+            displayToast("You've clicked play");
+        }else{
+            displayToast("You've clicked fast forward");
+        }
+
+
+    }
+
+    private void displayToast(String message) {
+
+        // Will prevent toast from showing if tapping the button a lot.
+        if (toast == null || toast.getView().getWindowVisibility() != View.VISIBLE) {
+            toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
