ad27f0b0211c6689d8fbfdda8c736cfaad7c7ba6
Nicholas LaBelle <nrlabelle@gmail.com>
2017-11-22 15:03:05 -0600

    Initial Song list file creation

---
+++ musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/SongListActivity.java
@@ -0,0 +1,11 @@
+package edu.cvtc.android.capstonemusic;
+
+import android.app.ListActivity;
+
+/**
+ * Created by Nicholas on 11/22/17.
+ */
+
+public class SongListActivity extends ListActivity {
+
+}
