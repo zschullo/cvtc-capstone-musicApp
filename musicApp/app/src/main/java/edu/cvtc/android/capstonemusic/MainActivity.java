ed91535ecc1b12e04b3ac90d23a56d0d34ecbb11
klicker22 <aklick_22@hotmail.com>
2017-11-29 13:20:06 -0600

    Initial location tracking and map

--- musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MainActivity.java
+++ musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MainActivity.java
@@ -1,18 +1,22 @@
 package edu.cvtc.android.capstonemusic;
 
+import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.maps.MapView;
 
 import java.io.Console;
 import java.lang.reflect.Field;
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageButton playButton;
     private ImageButton fastForwardButton;
     private ImageButton reverseButton;
+    private ImageButton mapButton;
 
     private TextView timeLabel;
     private TextView totalTimeLabel;
@@ -38,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Handler mHandler = new Handler();
 
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +55,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         playButton = (ImageButton) findViewById(R.id.playButton);
         fastForwardButton = (ImageButton) findViewById(R.id.fastForwardButton);
         reverseButton = (ImageButton) findViewById(R.id.reverseButton);
+        mapButton = (ImageButton) findViewById(R.id.mapButton);
         seekBar = (SeekBar) findViewById(R.id.musicBar);
         timeLabel = (TextView) findViewById(R.id.timeInitial);
         totalTimeLabel = (TextView) findViewById(R.id.timeTotal);
@@ -58,6 +65,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fastForwardButton.setOnClickListener(this);
         reverseButton.setOnClickListener(this);
         seekBar.setOnSeekBarChangeListener(this);
+        mapButton.setOnClickListener(this);
 
 
         setupMusic(R.raw.arma_puros_plus_nothing_else);
@@ -76,6 +84,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+
+
     }
 
     // This will get fired off when you click play and any other button.
@@ -92,7 +102,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mediaPlayer.start();
                 playButton.setImageResource(R.drawable.pause);
             }
+        } else if (view == mapButton) {
+            displayToast("The map button was pressed");
+            Intent intent = new Intent(this, MapsActivity.class);
+            startActivity(intent);
         }
+
+
     }
 
     private void displayToast(String message) {
@@ -179,7 +195,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         database.genreDAO().addGenre(new Genre(4, "Jazz"));
 
         genre = database.genreDAO().getAllGenres().get(3);
-        displayToast(genre.genreName);
+        //displayToast(genre.genreName);
 
         MediaMetadataRetriever songMetaData = new MediaMetadataRetriever();
 
@@ -215,7 +231,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     0));
 
             music = database.musicDAO().getMusic(count).get(0);
-            displayToast("Added " + music.title + ", GenreId: " + music.genreId);
+            //displayToast("Added " + music.title + ", GenreId: " + music.genreId);
         }
 
     }
