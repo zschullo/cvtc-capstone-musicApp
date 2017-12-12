a1d4b0ac2ee5b831b0771faef767a1f36e62dff7
klicker22 <aklick_22@hotmail.com>
2017-12-12 00:17:46 -0800

    Location listener and permissions working properly

--- musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MainActivity.java
+++ musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.cvtc.android.capstonemusic;
 
+import android.*;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,7 +12,9 @@ import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -66,6 +70,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getPermissionToUseLocationServices();
+
         songImage = (ImageView) findViewById(R.id.imageView);
 
         // Gets the id after the main activity is created.
@@ -112,6 +118,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
+    }
+
+    // Identifier for the permission request
+    private static final int ACCESS_FINE_LOCATION_PERMISSION_REQUEST = 1;
+
+    // Called when the user is performing an action which requires the app to read the
+    // user's contacts
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void getPermissionToUseLocationServices() {
+        // 1) Use the support library version ContextCompat.checkSelfPermission(...) to avoid
+        // checking the build version since Context.checkSelfPermission(...) is only available
+        // in Marshmallow
+        // 2) Always check for permission (even if permission has already been granted)
+        // since the user can revoke permissions at any time through Settings
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // The permission is NOT already granted.
+            // Check if the user has been asked about this permission already and denied
+            // it. If so, we want to give more explanation about why the permission is needed.
+            if (shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show our own UI to explain to the user why we need to read the contacts
+                // before actually requesting the permission and showing the default UI
+            }
+
+            // Fire off an async request to actually get the permission
+            // This will show the standard permission request dialog UI
+            requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION_PERMISSION_REQUEST);
+
+        }
+
+    }
+
+    // Callback with the request from calling requestPermissions(...)
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+
+        if (requestCode == ACCESS_FINE_LOCATION_PERMISSION_REQUEST) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                displayToast("Location permission granted");
+            } else {
+                // showRationale = false if user clicks Never Ask Again, otherwise true
+                boolean showRationale = shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+                if (showRationale) {
+
+                } else {
+                    displayToast("Location permission denied");
+                }
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     // This will get fired off when you click play and any other button.
--- musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MapsActivity.java
+++ musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MapsActivity.java
@@ -1,43 +1,24 @@
 package edu.cvtc.android.capstonemusic;
 
 import android.annotation.SuppressLint;
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        com.google.android.gms.location.LocationListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private TextView playTextField;
     private LatLng latLng;
@@ -46,19 +27,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     public static final String TAG = MapsActivity.class.getSimpleName();
 
-    /*
-     * Define a request code to send to Google Play services
-     * This code is returned in Activity.onActivityResult
-     */
-    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-
     private GoogleMap mMap;
-
-    private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
-
     private Location currentLocation;
+    private Location lastLocation;
+    LocationManager locationManager;
 
+    @SuppressLint("MissingPermission")
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,174 +41,69 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         playTextField = (TextView) findViewById(R.id.play);
 
-        getPermissionToUseLocationServices();
-
-//        mGoogleApiClient = new GoogleApiClient.Builder(this)
-//                .addConnectionCallbacks(this)
-//                .addOnConnectionFailedListener(this)
-//                .addApi(LocationServices.API)
-//                .build();
-
-        // Create the LocationRequest object
-        mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
-                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        locationManager = (LocationManager) getSystemService(this.LOCATION_SERVICE);
 
-    }
+        LocationListener locationListener = new LocationListener() {
 
-    // Identifier for the permission request
-    private static final int ACCESS_FINE_LOCATION_PERMISSION_REQUEST = 1;
+            @Override
+            public void onLocationChanged(Location location) {
+                lastLocation = currentLocation;
+                displayToast("Location Changed");
+                displayToast("Last location = " + location.getLatitude() + " and Longitude = " + location.getLongitude());
+                currentLocation = location;
+//                float distanceTraveled = lastLocation.distanceTo(location);
+//                displayToast("Distance = " + distanceTraveled);
+                latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                playTextField.setText("Latitude = " + location.getLatitude() + " and Longitude = " + location.getLongitude());
 
-    // Called when the user is performing an action which requires the app to read the
-    // user's contacts
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public void getPermissionToUseLocationServices() {
-        // 1) Use the support library version ContextCompat.checkSelfPermission(...) to avoid
-        // checking the build version since Context.checkSelfPermission(...) is only available
-        // in Marshmallow
-        // 2) Always check for permission (even if permission has already been granted)
-        // since the user can revoke permissions at any time through Settings
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            // The permission is NOT already granted.
-            // Check if the user has been asked about this permission already and denied
-            // it. If so, we want to give more explanation about why the permission is needed.
-            if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Show our own UI to explain to the user why we need to read the contacts
-                // before actually requesting the permission and showing the default UI
+                updateMap();
             }
 
-            // Fire off an async request to actually get the permission
-            // This will show the standard permission request dialog UI
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION_PERMISSION_REQUEST);
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
 
-        }
-
-    }
-
-    // Callback with the request from calling requestPermissions(...)
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-
-        if (requestCode == ACCESS_FINE_LOCATION_PERMISSION_REQUEST) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                displayToast("Location permission granted");
-            } else {
-                // showRationale = false if user clicks Never Ask Again, otherwise true
-                boolean showRationale = shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION);
-
-                if (showRationale) {
-
-                } else {
-                    displayToast("Location permission denied");
-                }
             }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
 
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000,1,locationListener);
+
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mGoogleApiClient.connect();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
-        // Where you would pause location services but we will keep it running for now to continue
-        // to track you as you move even if in background.
-        // mGoogleApiClient.disconnect();
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        if (currentLocation != null) {
-            // Add a marker in Sydney and move the camera
-            LatLng currentLoc = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(currentLoc).title("Your current location dude"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLoc));
-        }
     }
 
-
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-
-        FusedLocationProviderClient mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
-        mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                // GPS location can be null if GPS is switched off
-                if (location != null) {
-                    onLocationChanged(location);
-                }
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.d("TAG", "Error trying to get last GPS location");
-                e.printStackTrace();
-            }
-        });
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-        @Override
-        public void onLocationChanged (Location location){
-            displayToast("Location Changed");
-            playTextField.setText("Latitude = " + location.getLatitude() + " and Longitude = " + location.getLongitude());
-            currentLocation = location;
-            latLng = new LatLng(location.getLatitude(), location.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(latLng).title("You are here"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            Log.i(TAG, "Location changed");
-
-        }
-    @SuppressLint("MissingPermission")
-    public void getCurrentLocation() {
-        FusedLocationProviderClient mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
-        mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                // GPS location can be null if GPS is switched off
-                if (location != null) {
-                    onLocationChanged(location);
-                }
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.d("TAG", "Error trying to get last GPS location");
-                e.printStackTrace();
-            }
-        });
+    public void updateMap() {
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(latLng).title("You are here"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), 20));
     }
 
     private void displayToast(String message) {
