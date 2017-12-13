9f65822f86b608b64a260a335ebc9e35045c563d
Zachary Schullo <zschullo@student.cvtc.edu>
2017-12-13 14:21:03 -0600

    widget update

--- musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MainActivity.java
+++ musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MainActivity.java
@@ -1,6 +1,5 @@
 package edu.cvtc.android.capstonemusic;
 
-import android.*;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
@@ -11,12 +10,12 @@ import android.graphics.drawable.Drawable;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
--- musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MyAppWidgetProvider.java
+++ musicApp/app/src/main/java/edu/cvtc/android/capstonemusic/MyAppWidgetProvider.java
@@ -1,14 +1,29 @@
 package edu.cvtc.android.capstonemusic;
-import android.appwidget.AppWidgetProvider;
-import android.util.Log;
-import android.widget.RemoteViews;
-import android.content.Context;
+
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
 
 public class MyAppWidgetProvider extends AppWidgetProvider {
 
+    public static String PLAY_ACTION = "PlayAction";
+
+    /*
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.sound);
+        mp.start();
+    }
+    */
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        //https://stackoverflow.com/questions/2748590/clickable-widgets-in-android
 
         final int count = appWidgetIds.length;
 
@@ -16,16 +31,44 @@ public class MyAppWidgetProvider extends AppWidgetProvider {
 
             int widgetId = appWidgetIds[i];
 
-            Log.d("MSG", "Made it");
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, MyAppWidgetProvider.class);
+            intent.setAction(PLAY_ACTION);
 
-            // Retrieve the widget’s layout//
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener to the buttons
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
 
-            // Tell the AppWidgetManager to perform an update on this application widget//
+            views.setOnClickPendingIntent(R.id.playButton, pendingIntent);
+            //views.setOnClickPendingIntent(R.id.forwardButton, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(widgetId, views);
 
         }
     }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        //MediaPlayer mp = new MediaPlayer();
+
+        super.onReceive(context, intent);
+
+        if (intent.getAction().equals(PLAY_ACTION)) {
+
+
+
+        }
+
+    }
+
+
+
+
+
+
 
 
 }
